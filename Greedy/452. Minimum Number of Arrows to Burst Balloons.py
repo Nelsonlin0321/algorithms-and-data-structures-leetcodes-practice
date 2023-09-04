4452e1e56ca7d310eb0604d6c968ede53e4fcee3
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-09-04 18:39:41 +0800

    add 452. Minimum Number of Arrows to Burst Balloons

---
+++ Greedy/452. Minimum Number of Arrows to Burst Balloons.py
@@ -0,0 +1,54 @@
+from typing import List
+
+"""
+Runtime
+Details
+1234ms
+Beats 44.36%of users with Python3
+Memory
+Details
+62.61MB
+Beats 73.58%of users with Python3
+"""
+
+
+class Solution:
+
+    def get_overlap(self, left, right):
+        if right[0] <= left[1]:
+            return [right[0], left[1]]
+        return []
+
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+
+        # base case
+        num_points = len(points)
+        if num_points <= 1:
+            return num_points
+
+        points = sorted(points, key=lambda x: x[0])
+        points = sorted(points, key=lambda x: x[1])
+        print(f"points:{points}")
+
+        left_point = points[0]
+        i = 1
+        n_arrow = 1
+
+        while i < num_points:
+            right_point = points[i]
+            left_point = self.get_overlap(left_point, right_point)
+
+            if not left_point:  # not overlapping
+                n_arrow += 1
+                left_point = right_point
+
+            i += 1
+
+        return n_arrow
+
+
+if __name__ == "__main__":
+    points = [[3, 9], [7, 12], [3, 8], [6, 8], [
+        9, 10], [2, 9], [0, 9], [3, 9], [0, 6], [2, 8]]
+    res = Solution().findMinArrowShots(points)
+    print(res)
