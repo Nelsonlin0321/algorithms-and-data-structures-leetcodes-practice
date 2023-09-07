39830bc3d3255b2e5f2c3d3e07fc20d2b2686b59
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-09-07 23:57:18 +0800

    633. Sum of Square Numbers

---
+++ TwoPointers/633. Sum of Square Numbers.py
@@ -0,0 +1,41 @@
+"""
+Runtime
+Details
+319ms
+Beats 32.50%of users with Python3
+Memory
+Details
+16.31MB
+Beats 37.07%of users with Python3
+"""
+
+
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+
+        left = 0
+        right = int(c**(1/2))  # upper limit
+
+        while left <= right:
+            sqr_sum = left**2+right**2
+
+            if sqr_sum == c:
+                return True
+
+            if sqr_sum > c:
+                right -= 1
+            elif sqr_sum < c:
+                left += 1
+
+        return False
+
+
+if __name__ == "__main__":
+    import time
+    c = 100000000
+    start = time.time()
+    res = Solution().judgeSquareSum(c)
+    end = time.time()
+    spent = round((end-start)*1000, 2)
+    print(res)
+    print(f"Spent:{spent} s")
