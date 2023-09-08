46dca848b3eb89c1c4de3c5f248e09a3790b1a4c
Nelsonlin0321 <nelsonlin0321@outlook.com>
2023-09-08 18:44:11 +0800

    add math leetcode

---
+++ Math/2843. Count Symmetric Integers.py
@@ -0,0 +1,29 @@
+
+def is_symmetric(number):
+    str_number = str(number)
+    if len(str_number) % 2 == 1:
+        return False
+
+    i = len(str_number) // 2
+
+    left_str = str_number[0:i]
+    left_sum = sum([int(n) for n in left_str])
+    right_str = str_number[i:]
+    right_sum = sum([int(n) for n in right_str])
+
+    return left_sum == right_sum
+
+
+class Solution:
+    def countSymmetricIntegers(self, low: int, high: int) -> int:
+        count = 0
+        for n in range(low, high+1):
+            if is_symmetric(n):
+                count += 1
+
+        return count
+
+
+if __name__ == "__main__":
+    res = Solution().countSymmetricIntegers(low=1200, high=1230)
+    print(res)
---
+++ Math/2844. Minimum Operations to Make a Special Number.py
@@ -0,0 +1,51 @@
+"""
+Submission Detail
+811 / 811 test cases passed.
+	Status: Accepted
+Runtime: 90 ms
+Memory Usage: 16.1 MB
+"""
+
+
+def is_qualified(nums):
+    match_suffix = {"25", "50", "75", "00"}
+    for i in range(len(nums)-1):
+        for j in range(i+1, len(nums)):
+            suffix = nums[i]+nums[j]
+            if suffix in match_suffix:
+                return True
+    return False
+
+
+class Solution:
+    def minimumOperations(self, num: str) -> int:
+
+        if len(num) == 1:
+            if num == '0':
+                return 0
+            else:
+                return 1
+
+        if len(num) == 2:
+            if num in {"25", "50", "75"}:
+                return 0
+            if num[-1] == "0":
+                return 1
+
+        check_suffix = {"2", "5", "0", "7"}
+        for i in range(len(num)-2, -1, -1):
+            n = num[i]
+            if n in check_suffix:
+                sub_num = num[i:]
+                if is_qualified(sub_num):
+                    return len(sub_num) - 2
+
+        if "0" in num:
+            return len(num)-1
+        else:
+            return len(num)
+
+
+if __name__ == "__main__":
+    res = Solution().minimumOperations(num="2245047")
+    print(res)
