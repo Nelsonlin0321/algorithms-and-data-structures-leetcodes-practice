fab99e247e2e7a611a86489ec6ce38c479c38a0d
Nelson Lin <nelsonlin0321@outlook.com>
2025-06-01 18:33:55 +0800

    Add dynamic programming solution for Regular Expression Matching with memoization to optimize performance and prevent redundant calculations.

--- DynamicProgramming-2D/10. Regular Expression Matching.py
+++ DynamicProgramming-2D/10. Regular Expression Matching.py
@@ -16,24 +16,48 @@ p[j] 只能匹配 0 次，然后看下一个字符是否能和 s[i] 匹配。比
 
 
 class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
-        
-        i = 0
-        j = 0
-        
-        
-        while i< len(s) and j < len(p):
-            
-            # match
-            if s[i] == p[j] or p[j]=='.':
-                
-                if j+1 > len(p) and p[j+1] =="*":
-                    # can match 0 for more
-                    pass
-        pass
-    
+    # 备忘录
+    def __init__(self):
+        self.memo = None
 
-            
-            
-    
-        
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+        self.memo = [[-1 for _ in range(n)] for _ in range(m)]
+        # 指针 i，j 从索引 0 开始移动
+        return self.dp(s, 0, p, 0)
+
+    # 计算 p[j..] 是否匹配 s[i..]
+    def dp(self, s: str, i: int, p: str, j: int) -> bool:
+        m, n = len(s), len(p)
+        # base case
+        if j == n:
+            return i == m
+        if i == m:
+            if (n - j) % 2 == 1:
+                return False
+            for j in range(j, n, 2):
+                if p[j + 1] != '*':
+                    return False
+            return True
+
+        # 查备忘录，防止重复计算
+        if self.memo[i][j] != -1:
+            return self.memo[i][j] == 1
+
+        res = False
+        if s[i] == p[j] or p[j] == '.':
+            if j < n - 1 and p[j + 1] == '*':
+                # 匹配 0 次, 或者 匹配 多 次， 忽略当前字符 i
+                res = self.dp(s, i, p, j + 2) or self.dp(s, i + 1, p, j)
+            else:
+                res = self.dp(s, i + 1, p, j + 1)
+        else:
+            # 不匹配， 且 p[j + 1] 是 *， * 匹配0次， p 跳過當前字符 j 和 j+1 *
+            if j < n - 1 and p[j + 1] == '*':
+                res = self.dp(s, i, p, j + 2)
+            else:
+                res = False
+
+        # 将当前结果记入备忘录
+        self.memo[i][j] = 1 if res else 0
+        return res
